0fbc9116711b9c1eaaafd3f69d7ed670976dde2c
Antony Cherepanov <antony.cherepanov@gmail.com>
2016-11-09 11:55:55 +0300

    AndroidUI: play with visibility parameter

--- AndroidUI/app/src/main/java/com/iamantony/androidui/MainActivity.java
+++ AndroidUI/app/src/main/java/com/iamantony/androidui/MainActivity.java
@@ -2,6 +2,9 @@ package com.iamantony.androidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.AnalogClock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AnalogClock analogClock = (AnalogClock)findViewById(R.id.aClock);
+        final Button clockButton = (Button)findViewById(R.id.clockButton);
+        clockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (analogClock.isShown()) {
+                    analogClock.setVisibility(View.INVISIBLE);
+                    clockButton.setText(R.string.clockBtnShow);
+                }
+                else {
+                    analogClock.setVisibility(View.VISIBLE);
+                    clockButton.setText(R.string.clockBtnHide);
+                }
+
+            }
+        });
     }
 }
